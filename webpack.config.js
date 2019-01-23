f1fe8b18c1fcd198ef7d439d56a6e849412c9e2b
William Fischer <william.fischer42@gmail.com>
2019-01-23 15:25:21 -0700

    Fix more webpack and package errors preventing server from starting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
+    test: "mocha-loader!./test/index.js"
   },
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.css', '.scss']
+    extensions: ['.js', '.json', '.css', '.scss']
   }
 };
